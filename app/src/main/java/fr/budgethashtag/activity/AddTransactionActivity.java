6721b21322b4582ec3ba3dfbc209d22e919a44cb
924421 <glaurent@spir.fr>
2018-06-25 17:54:28 +0200

    correction build

--- app/src/main/java/fr/budgethashtag/activity/AddTransactionActivity.java
+++ app/src/main/java/fr/budgethashtag/activity/AddTransactionActivity.java
@@ -3,6 +3,8 @@ package fr.budgethashtag.activity;
 import android.app.Activity;
 import android.os.Bundle;
 
+import fr.budgethashtag.R;
+
 public class AddTransactionActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
