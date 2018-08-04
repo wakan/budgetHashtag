e6feb31d53a6f0659e37732e59ce3ab9888df295
WaKaN <wakan_powa_s01@hotmail.fr>
2018-08-04 14:53:39 +0200

    test observable

--- app/src/androidTest/java/fr/budgethashtag/PortefeuilleInstrumentedTest.java
+++ app/src/androidTest/java/fr/budgethashtag/PortefeuilleInstrumentedTest.java
@@ -3,12 +3,23 @@ package fr.budgethashtag;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import fr.budgethashtag.basecolumns.Budget;
 import fr.budgethashtag.service.portefeuille.PortefeuilleService;
 import fr.budgethashtag.service.portefeuille.PortefeuilleServiceImpl;
+import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Predicate;
+import io.reactivex.observers.TestObserver;
+import io.reactivex.plugins.RxJavaPlugins;
+import io.reactivex.schedulers.TestScheduler;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -20,32 +31,41 @@ import static org.junit.Assert.assertTrue;
  */
 @RunWith(AndroidJUnit4.class)
 public class PortefeuilleInstrumentedTest {
+    @Before
+    public void SetUp() {
+        RxJavaPlugins.reset();
+    }
+    @After
+    public void tearDown(){
+        RxJavaPlugins.reset();
+    }
+
     @Test
-    public void getOrCreatePortefeuilleFirstTest() {
+    public void getOrCreatePortefeuilleFirstTest() throws InterruptedException {
         getOrCreatePortefeuilleTest();
     }
     @Test
-    public void getOrCreatePortefeuilleSecondTest() {
+    public void getOrCreatePortefeuilleSecondTest() throws InterruptedException {
         getOrCreatePortefeuilleTest();
     }
     Long idPorteuille = -1L;
-    private void getOrCreatePortefeuilleTest() {
+    private void getOrCreatePortefeuilleTest() throws InterruptedException {
         Context appContext = InstrumentationRegistry.getTargetContext();
-        BudgetHashtagApplication.instance.getServiceManager().getPortefeuilleService()
+        Observable<Long> obs = BudgetHashtagApplication.instance.getServiceManager().getPortefeuilleService()
                 .getOrCreateDefaultPortefeuilleIfNotExistAsync(appContext)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<Long>() {
-                    @Override
-                    public void accept(Long id) throws Exception {
-                        if (idPorteuille < 0) {
-                            idPorteuille = id;
-                            assertTrue(idPorteuille >= 0);
-                        }
-                        else {
-                            assertEquals(idPorteuille, id);
-                        }
-                    }
-                });
+                .observeOn(AndroidSchedulers.mainThread());
+        TestObserver<Long> testObserver = obs.test();
+        testObserver.awaitDone(15, TimeUnit.SECONDS);
+        testObserver.assertValue(new Predicate<Long>() {
+            @Override
+            public boolean test(Long id) throws Exception {
+                if (idPorteuille < 0) {
+                    idPorteuille = id;
+                    return idPorteuille >= 0;
+                } else {
+                    return idPorteuille == id;
+                }
+            }
+        });
     }
-
 }
